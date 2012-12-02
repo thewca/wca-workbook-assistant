d7873107e98c44dfc413948bb8f451046759f392
Lars Vandenbergh <lars.vandenbergh@telenet.be>
2012-12-02 15:36:00 +0100

    Make sure formula cells are re-evaluated if their cached value is empty. #17

--- src/main/java/org/worldcubeassociation/ui/WorkbookTableDataExtractor.java
+++ src/main/java/org/worldcubeassociation/ui/WorkbookTableDataExtractor.java
@@ -3,10 +3,12 @@ package org.worldcubeassociation.ui;
 import org.apache.poi.ss.format.CellFormat;
 import org.apache.poi.ss.format.CellFormatResult;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.worldcubeassociation.workbook.MatchedSheet;
 import org.worldcubeassociation.workbook.MatchedWorkbook;
+import org.worldcubeassociation.workbook.parse.CellParser;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -28,6 +30,7 @@ public class WorkbookTableDataExtractor {
     public static void extractTableData(MatchedSheet aMatchedSheet) {
         // Determine the row and column range that has usable data.
         Sheet sheet = aMatchedSheet.getSheet();
+        FormulaEvaluator evaluator = sheet.getWorkbook().getCreationHelper().createFormulaEvaluator();
         int lastRowNum = sheet.getLastRowNum();
         int lastNonEmptyRow = 0;
         int lastNonEmptyCol = 0;
@@ -41,8 +44,8 @@ public class WorkbookTableDataExtractor {
             for (int cellIdx = 0; cellIdx < MAX_COLS; cellIdx++) {
                 Cell cell = row.getCell(cellIdx);
                 if (cell != null) {
-                    String dataFormat = cell.getCellStyle().getDataFormatString();
-                    CellFormat cellFormat = CellFormat.getInstance(dataFormat);
+                    String cellFormatString = CellParser.getCellFormatString(cell);
+                    CellFormat cellFormat = CellFormat.getInstance(cellFormatString);
                     CellFormatResult formatResult = cellFormat.apply(cell);
                     if (formatResult.text != null && !formatResult.text.equals("")) {
                         rowHasNonEmptyCell = true;
@@ -69,16 +72,31 @@ public class WorkbookTableDataExtractor {
             for (int cellIdx = 0; cellIdx <= lastNonEmptyCol; cellIdx++) {
                 Cell cell = row.getCell(cellIdx);
                 if (cell != null) {
-                    String dataFormat = cell.getCellStyle().getDataFormatString();
-                    if (CellFormat.ultimateType(cell) == Cell.CELL_TYPE_NUMERIC && dataFormat.toUpperCase().contains(":SS")) {
+                    String cellFormatString = CellParser.getCellFormatString(cell);
+                    CellFormat cellFormat = CellFormat.getInstance(cellFormatString);
+                    CellFormatResult formatResult = cellFormat.apply(cell);
+
+                    // This is to make sure that only formulas with no cached result are evaluated again.
+                    if (cell.getCellType() == Cell.CELL_TYPE_FORMULA &&
+                            (formatResult.text == null || formatResult.text.equals(""))) {
+                        try {
+                            evaluator.evaluateFormulaCell(cell);
+                            formatResult = cellFormat.apply(cell);
+                        }
+                        catch (Exception e) {
+                            System.out.println("Error evaluating formula in sheet " +
+                                    sheet.getSheetName() + ", row " + (rowIdx + 1) +
+                                    ", col " + (cellIdx + 1));
+                        }
+                    }
+
+                    if (CellFormat.ultimateType(cell) == Cell.CELL_TYPE_NUMERIC && cellFormatString.toUpperCase().contains(":SS")) {
                         Date dateCellValue = cell.getDateCellValue();
                         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("m:ss.SSS");
                         String formattedDate = simpleDateFormat.format(dateCellValue);
                         data[rowIdx][cellIdx + 1] = formattedDate.substring(0, formattedDate.length() - 1);
                     }
                     else {
-                        CellFormat cellFormat = CellFormat.getInstance(dataFormat);
-                        CellFormatResult formatResult = cellFormat.apply(cell);
                         data[rowIdx][cellIdx + 1] = formatResult.text;
                     }
                 }
