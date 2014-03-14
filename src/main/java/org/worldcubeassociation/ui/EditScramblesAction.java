6ad4c215e2bec9fc28e7904e17faab3977c78d08
jazzthief81 <lars.vandenbergh@telenet.be>
2014-03-14 23:36:04 +0100

    UI improvements. #73

--- src/main/java/org/worldcubeassociation/ui/EditScramblesAction.java
+++ src/main/java/org/worldcubeassociation/ui/EditScramblesAction.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-import javax.swing.AbstractAction;
+import javax.swing.*;
 
 import org.worldcubeassociation.WorkbookAssistantEnv;
 import org.worldcubeassociation.ui.tree.TreeDragAndDrop;
@@ -23,12 +23,12 @@ public class EditScramblesAction extends AbstractAction implements PropertyChang
     private TreeDragAndDrop scrambleTree;
 
     public EditScramblesAction(WorkbookAssistantEnv aEnv) {
-        super("Edit");
+        super("Edit...");
         fEnv = aEnv;
         aEnv.addPropertyChangeListener(this);
         initUI();
         updateEnabledState();
-        
+
     }
 
     private void updateEnabledState() {
@@ -39,17 +39,39 @@ public class EditScramblesAction extends AbstractAction implements PropertyChang
 
     private void initUI() {
         fDialog = new NicelySizedJDialog(fEnv.getTopLevelComponent(), "Edit scrambles", Dialog.ModalityType.APPLICATION_MODAL);
-        
+        fDialog.getContentPane().setLayout(new GridBagLayout());
+
         scrambleTree = new TreeDragAndDrop(fEnv);
-        fDialog.getContentPane().add(scrambleTree.getContent());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        c.insets.top = 4;
+        c.insets.bottom = 4;
+        c.insets.left = 4;
+        c.insets.right = 4;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+
+        fDialog.getContentPane().add(scrambleTree.getContent(), c);
+
+        JPanel buttons = new JPanel();
+        fDialog.getContentPane().add(buttons, c);
+        buttons.add(new JButton(new OKAction()));
+        c.insets.top = 0;
+        c.gridy++;
+        c.weighty = 0;
+        c.anchor = GridBagConstraints.EAST;
+        c.fill = GridBagConstraints.VERTICAL;
+
+        fDialog.getContentPane().add(buttons, c);
     }
 
     @Override
     public void actionPerformed(ActionEvent event) {
-
         scrambleTree.expandTree();
         fDialog.pack();
-        
+
         fDialog.setLocationRelativeTo(fEnv.getTopLevelComponent());
         fDialog.setVisible(true);
     }
@@ -59,6 +81,19 @@ public class EditScramblesAction extends AbstractAction implements PropertyChang
         if (WorkbookAssistantEnv.SCRAMBLES.equals(e.getPropertyName())) {
             updateEnabledState();
         }
+    }
+
+    private class OKAction extends AbstractAction {
+
+        private OKAction() {
+            super("OK");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            fDialog.setVisible(false);
+        }
+
     }
 
 }
