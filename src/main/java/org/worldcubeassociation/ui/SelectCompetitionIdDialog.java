4262ae63170ce0fc83d8f2c99654cfad26adac34
jazzthief81 <lars.vandenbergh@telenet.be>
2015-03-11 21:06:48 +0100

    Improve overall behavior of competition ID selection. #100

--- src/main/java/org/worldcubeassociation/ui/SelectCompetitionIdDialog.java
+++ src/main/java/org/worldcubeassociation/ui/SelectCompetitionIdDialog.java
@@ -6,26 +6,30 @@ import org.worldcubeassociation.db.Competition;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.*;
-import java.util.List;
 
 /**
  * A dialog for selecting the competition ID, that allows searching a competition based on a keyword.
  */
 public class SelectCompetitionIdDialog extends JDialog {
 
+    private static final Calendar CALENDAR = Calendar.getInstance();
     private static long ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
     private WorkbookAssistantEnv fEnv;
-    private final JTextField fFilterTextField;
-    private final JList fCompetitionList;
+    private DialogKeyEventDispatcher fKeyEventDispatcher;
+    private JTextField fFilterTextField;
+    private JList fCompetitionList;
     private JCheckBox fOnlyShowRecentCheckBox;
     private int fSelectedOption;
-    private List<Competition> fSortedCompetitions;
-    private List<Competition> fSortedRecentCompetitions;
-    public static final Calendar CALENDAR = Calendar.getInstance();
+    private Vector<Competition> fSortedCompetitions;
+    private Vector<Competition> fSortedRecentCompetitions;
+    private Vector<Competition> fFilteredCompetitions;
+    private JButton fOkButton;
+    private JButton fCancelButton;
 
     public SelectCompetitionIdDialog(Frame aFrame, WorkbookAssistantEnv aEnv) {
         super(aFrame, "Select competition ID", true);
@@ -43,7 +47,7 @@ public class SelectCompetitionIdDialog extends JDialog {
         c.weightx = 0;
         c.weighty = 0;
         Container contentPane = getContentPane();
-        contentPane.add(new JLabel("Filter:"), c);
+        contentPane.add(new JLabel("Search:"), c);
 
         c.gridx++;
         c.weightx = 1;
@@ -72,6 +76,21 @@ public class SelectCompetitionIdDialog extends JDialog {
         c.gridwidth = 2;
         fCompetitionList = new JList();
         fCompetitionList.setCellRenderer(new CompetitionListCellRenderer());
+        fCompetitionList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        fCompetitionList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                updateOkButtonState();
+            }
+        });
+        fCompetitionList.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount() == 2) {
+                    fOkButton.doClick();
+                }
+            }
+        });
         contentPane.add(new JScrollPane(fCompetitionList), c);
 
         c.insets.bottom = 4;
@@ -83,10 +102,10 @@ public class SelectCompetitionIdDialog extends JDialog {
         JPanel buttonPanel = createButtonPanel();
         contentPane.add(buttonPanel, c);
 
-        fSortedCompetitions = new ArrayList<Competition>(fEnv.getDatabase().getCompetitions().getList());
+        fSortedCompetitions = new Vector<Competition>(fEnv.getDatabase().getCompetitions().getList());
         Collections.sort(fSortedCompetitions, new CompetitionDateComparator());
 
-        fSortedRecentCompetitions = new ArrayList<Competition>();
+        fSortedRecentCompetitions = new Vector<Competition>();
         long now = System.currentTimeMillis();
         for (Competition competition : fSortedCompetitions) {
             long competitionTime = dateOf(competition).getTime();
@@ -95,7 +114,22 @@ public class SelectCompetitionIdDialog extends JDialog {
             }
         }
 
+        fFilteredCompetitions = new Vector<Competition>(fSortedCompetitions.size());
+
         updateCompetitionList();
+
+        fKeyEventDispatcher = new DialogKeyEventDispatcher();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+                KeyboardFocusManager.getCurrentKeyboardFocusManager().removeKeyEventDispatcher(fKeyEventDispatcher);
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+                KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(fKeyEventDispatcher);
+            }
+        });
 
         pack();
     }
@@ -116,13 +150,16 @@ public class SelectCompetitionIdDialog extends JDialog {
         });
         buttonPanel.add(fOnlyShowRecentCheckBox, c);
         c.weightx = 0;
-        buttonPanel.add(new JButton(new OKAction()), c);
-        buttonPanel.add(new JButton(new CancelAction()), c);
+        fOkButton = new JButton(new OKAction());
+        updateOkButtonState();
+        buttonPanel.add(fOkButton, c);
+        fCancelButton = new JButton(new CancelAction());
+        buttonPanel.add(fCancelButton, c);
         return buttonPanel;
     }
 
     private void updateCompetitionList() {
-        List<Competition> competitions;
+        Vector<Competition> competitions;
         if (fOnlyShowRecentCheckBox.isSelected()) {
             competitions = fSortedRecentCompetitions;
         }
@@ -132,23 +169,30 @@ public class SelectCompetitionIdDialog extends JDialog {
 
         String searchString = fFilterTextField.getText() == null ? null : fFilterTextField.getText().toUpperCase();
         if (searchString == null) {
-            fCompetitionList.setListData(competitions.toArray());
+            fCompetitionList.setListData(competitions);
         }
         else {
-            List<Competition> filteredCompetitions = new ArrayList<Competition>();
+            fFilteredCompetitions.clear();
             for (Competition competition : competitions) {
                 if (competition.getId().toUpperCase().contains(searchString)) {
-                    filteredCompetitions.add(competition);
+                    fFilteredCompetitions.add(competition);
                 }
                 else if (competition.getName().toUpperCase().contains(searchString)) {
-                    filteredCompetitions.add(competition);
+                    fFilteredCompetitions.add(competition);
                 }
                 else if (competition.getWcaDelegate().toUpperCase().contains(searchString)) {
-                    filteredCompetitions.add(competition);
+                    fFilteredCompetitions.add(competition);
                 }
             }
-            fCompetitionList.setListData(filteredCompetitions.toArray());
+            fCompetitionList.setListData(fFilteredCompetitions);
+            if (fFilteredCompetitions.size() == 1) {
+                fCompetitionList.setSelectedIndex(0);
+            }
         }
+    }
+
+    private void updateOkButtonState() {
+        fOkButton.setEnabled(!fCompetitionList.isSelectionEmpty());
     }
 
     public int getSelectedOption() {
@@ -198,6 +242,28 @@ public class SelectCompetitionIdDialog extends JDialog {
         @Override
         public int compare(Competition aCompetition1, Competition aCompetition2) {
             return dateOf(aCompetition1).compareTo(dateOf(aCompetition2));
+        }
+
+    }
+
+    private class DialogKeyEventDispatcher implements KeyEventDispatcher {
+
+        @Override
+        public boolean dispatchKeyEvent(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                if (fOkButton.isEnabled()) {
+                    fOkButton.doClick();
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                fCancelButton.doClick();
+                return true;
+            }
+            return false;
         }
 
     }
