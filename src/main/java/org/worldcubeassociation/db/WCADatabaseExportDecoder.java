c5352834e705a1c6a13642336b353bdd236d1fc2
Jeremy Fleischman <jeremyfleischman@gmail.com>
2017-09-25 10:13:11 -0700

    Revert "For now, we need to handle both versions of the wca database export."
    This reverts commit dcf237281a02a170b8296e9030233342037ee1fe because we have merged up https://github.com/thewca/worldcubeassociation.org/pull/1955.

--- src/main/java/org/worldcubeassociation/db/WCADatabaseExportDecoder.java
+++ src/main/java/org/worldcubeassociation/db/WCADatabaseExportDecoder.java
@@ -132,17 +132,11 @@ public class WCADatabaseExportDecoder {
 
         Scanner scanner = new Scanner(aInputStream, "UTF-8");
         scanner.useDelimiter("[\t\n\r\f]");
-        String firstLine = scanner.nextLine();
-        boolean hasLatitude = firstLine.contains("latitude");
+        scanner.nextLine();
         while (scanner.hasNext()) {
             String id = scanner.next();
             String name = scanner.next();
             String continentId = scanner.next();
-            if(hasLatitude) {
-                scanner.next(); // latitude
-                scanner.next(); // longitude
-                scanner.next(); // zoom
-            }
             String iso2 = scanner.next();
 
             Country country = new Country(id, name, continentId, iso2);
