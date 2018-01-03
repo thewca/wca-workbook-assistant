b631b409b7fb0e008d49c33dc8d68c180e2bd76b
Jeremy Fleischman <jeremyfleischman@gmail.com>
2018-01-03 12:25:32 -0800

    Update event formats to match the latest regulations.

--- src/main/java/org/worldcubeassociation/workbook/FormatValidator.java
+++ src/main/java/org/worldcubeassociation/workbook/FormatValidator.java
@@ -14,32 +14,40 @@ public class FormatValidator {
 
     static {
         ALLOWED_FORMATS = new HashMap<Event, Set<Format>>();
-        ALLOWED_FORMATS.put(_222, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_333, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_444, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_555, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_clock, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_minx, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_pyram, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_sq1, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_skewb, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_333oh, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
+        HashSet<Format> formats;
 
-        ALLOWED_FORMATS.put(_333ft, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, MEAN_OF_3)));
-        ALLOWED_FORMATS.put(_333fm, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, MEAN_OF_3)));
-        ALLOWED_FORMATS.put(_666, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, MEAN_OF_3)));
-        ALLOWED_FORMATS.put(_777, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, MEAN_OF_3)));
+        // https://www.worldcubeassociation.org/regulations/#9b1
+        formats = new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5));
+        ALLOWED_FORMATS.put(_333, formats);
+        ALLOWED_FORMATS.put(_222, formats);
+        ALLOWED_FORMATS.put(_444, formats);
+        ALLOWED_FORMATS.put(_555, formats);
+        ALLOWED_FORMATS.put(_333oh, formats);
+        ALLOWED_FORMATS.put(_333ft, formats);
+        ALLOWED_FORMATS.put(_clock, formats);
+        ALLOWED_FORMATS.put(_minx, formats);
+        ALLOWED_FORMATS.put(_pyram, formats);
+        ALLOWED_FORMATS.put(_skewb, formats);
+        ALLOWED_FORMATS.put(_sq1, formats);
 
-        ALLOWED_FORMATS.put(_333bf, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3)));
-        ALLOWED_FORMATS.put(_444bf, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3)));
-        ALLOWED_FORMATS.put(_555bf, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3)));
-        ALLOWED_FORMATS.put(_333mbf, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3)));
-        ALLOWED_FORMATS.put(_333mbf, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3)));
+        // https://www.worldcubeassociation.org/regulations/#9b2
+        formats = new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, MEAN_OF_3));
+        ALLOWED_FORMATS.put(_333fm, formats);
+        ALLOWED_FORMATS.put(_666, formats);
+        ALLOWED_FORMATS.put(_777, formats);
 
-        // Magic and Master Magic is not official anymore since 2013 but these were the allowed formats when they still
-        // were official.
-        ALLOWED_FORMATS.put(_magic, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
-        ALLOWED_FORMATS.put(_mmagic, new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5)));
+        // https://www.worldcubeassociation.org/regulations/#9b3
+        formats = new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3));
+        ALLOWED_FORMATS.put(_333bf, formats);
+        ALLOWED_FORMATS.put(_444bf, formats);
+        ALLOWED_FORMATS.put(_555bf, formats);
+        ALLOWED_FORMATS.put(_333mbf, formats);
+
+        // Magic and Master Magic are not official anymore since 2013 but these
+        // were the allowed formats when they still were official.
+        formats = new HashSet<Format>(Arrays.asList(BEST_OF_1, BEST_OF_2, BEST_OF_3, AVERAGE_OF_5));
+        ALLOWED_FORMATS.put(_magic, formats);
+        ALLOWED_FORMATS.put(_mmagic, formats);
     }
 
     public static boolean isValidRoundFormat(Format aFormat, Event aEvent) {
